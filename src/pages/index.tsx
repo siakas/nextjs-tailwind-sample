5fe09896189a535f3944b1d9d224ca826dd68a1f
Sanshiroh SAKAI <sakai.s@ekzm.co.jp>
2023-03-02 11:46:52 +0900

    add tailwind eslint and prettier setting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,25 +2,25 @@ const Home = () => {
   return (
     <>
       <div className="bg-white pb-6 sm:pb-8 lg:pb-12">
-        <div className="flex flex-wrap sm:flex-nowrap sm:justify-center sm:items-center bg-indigo-500 relative sm:gap-3 px-4 sm:pr-8 md:px-8 py-3">
-          <div className="order-1 sm:order-none w-11/12 sm:w-auto max-w-screen-sm inline-block text-white text-sm md:text-base mb-2 sm:mb-0">
+        <div className="relative flex flex-wrap bg-indigo-500 px-4 py-3 sm:flex-nowrap sm:items-center sm:justify-center sm:gap-3 sm:pr-8 md:px-8">
+          <div className="order-1 mb-2 inline-block w-11/12 max-w-screen-sm text-sm text-white sm:order-none sm:mb-0 sm:w-auto md:text-base">
             This is a section of some simple filler text, also known as
             placeholder text.
           </div>
           <a
-            href="#"
-            className="order-last sm:order-none w-full sm:w-auto inline-block bg-indigo-600 hover:bg-indigo-700 active:bg-indigo-800 focus-visible:ring ring-indigo-300 text-white text-xs md:text-sm font-semibold text-center whitespace-nowrap rounded-lg outline-none transition duration-100 px-4 py-2"
+            href=""
+            className="order-last inline-block w-full whitespace-nowrap rounded-lg bg-indigo-600 px-4 py-2 text-center text-xs font-semibold text-white outline-none ring-indigo-300 transition duration-100 hover:bg-indigo-700 focus-visible:ring active:bg-indigo-800 sm:order-none sm:w-auto md:text-sm"
           >
             Learn more
           </a>
-          <div className="order-2 sm:order-none w-1/12 sm:w-auto flex justify-end items-start sm:absolute sm:right-0 sm:mr-1 xl:mr-3">
+          <div className="order-2 flex w-1/12 items-start justify-end sm:absolute sm:right-0 sm:order-none sm:mr-1 sm:w-auto xl:mr-3">
             <button
               type="button"
-              className="text-white hover:text-indigo-100 active:text-indigo-200 transition duration-100"
+              className="text-white transition duration-100 hover:text-indigo-100 active:text-indigo-200"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="w-5 md:w-6 h-5 md:h-6"
+                className="h-5 w-5 md:h-6 md:w-6"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -38,31 +38,31 @@ const Home = () => {
       </div>
 
       <div className="bg-white py-6 sm:py-8 lg:py-12">
-        <div className="max-w-screen-2xl px-4 md:px-8 mx-auto">
-          <div className="flex justify-between items-end gap-4 mb-6">
-            <h2 className="text-gray-800 text-2xl lg:text-3xl font-bold">
+        <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
+          <div className="mb-6 flex items-end justify-between gap-4">
+            <h2 className="text-2xl font-bold text-gray-800 lg:text-3xl">
               Collections
             </h2>
 
             <a
               href="#"
-              className="inline-block bg-white hover:bg-gray-100 active:bg-gray-200 focus-visible:ring ring-indigo-300 border text-gray-500 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-2 md:py-3"
+              className="inline-block rounded-lg border bg-white px-4 py-2 text-center text-sm font-semibold text-gray-500 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-100 focus-visible:ring active:bg-gray-200 md:px-8 md:py-3 md:text-base"
             >
               Show more
             </a>
           </div>
 
-          <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-4 md:gap-x-6 gap-y-6">
+          <div className="grid gap-x-4 gap-y-6 sm:grid-cols-2 md:gap-x-6 lg:grid-cols-3 xl:grid-cols-4">
             <div>
               <a
                 href="#"
-                className="group h-96 block bg-gray-100 rounded-lg overflow-hidden shadow-lg mb-2 lg:mb-3"
+                className="group mb-2 block h-96 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3"
               >
                 <img
                   src="https://images.unsplash.com/photo-1552374196-1ab2a1c593e8?auto=format&q=75&fit=crop&crop=top&w=600&h=700"
                   loading="lazy"
                   alt="Photo by Austin Wade"
-                  className="w-full h-full object-cover object-center group-hover:scale-110 transition duration-200"
+                  className="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
                 />
               </a>
 
@@ -70,7 +70,7 @@ const Home = () => {
                 <span className="text-gray-500">Men</span>
                 <a
                   href="#"
-                  className="text-gray-800 hover:text-gray-500 text-lg lg:text-xl font-bold transition duration-100"
+                  className="text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl"
                 >
                   Business Causual
                 </a>
@@ -79,13 +79,13 @@ const Home = () => {
             <div>
               <a
                 href="#"
-                className="group h-96 block bg-gray-100 rounded-lg overflow-hidden shadow-lg mb-2 lg:mb-3"
+                className="group mb-2 block h-96 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3"
               >
                 <img
                   src="https://images.unsplash.com/photo-1603344797033-f0f4f587ab60?auto=format&q=75&fit=crop&crop=top&w=600&h=700"
                   loading="lazy"
                   alt="Photo by engin akyurt"
-                  className="w-full h-full object-cover object-center group-hover:scale-110 transition duration-200"
+                  className="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
                 />
               </a>
 
@@ -93,7 +93,7 @@ const Home = () => {
                 <span className="text-gray-500">Women</span>
                 <a
                   href="#"
-                  className="text-gray-800 hover:text-gray-500 text-lg lg:text-xl font-bold transition duration-100"
+                  className="text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl"
                 >
                   Summer Season
                 </a>
@@ -102,13 +102,13 @@ const Home = () => {
             <div>
               <a
                 href="#"
-                className="group h-96 block bg-gray-100 rounded-lg overflow-hidden shadow-lg mb-2 lg:mb-3"
+                className="group mb-2 block h-96 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3"
               >
                 <img
                   src="https://images.unsplash.com/photo-1552668693-d0738e00eca8?auto=format&q=75&fit=crop&crop=top&w=600&h=700"
                   loading="lazy"
                   alt="Photo by Austin Wade"
-                  className="w-full h-full object-cover object-center group-hover:scale-110 transition duration-200"
+                  className="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
                 />
               </a>
 
@@ -116,7 +116,7 @@ const Home = () => {
                 <span className="text-gray-500">Men</span>
                 <a
                   href="#"
-                  className="text-gray-800 hover:text-gray-500 text-lg lg:text-xl font-bold transition duration-100"
+                  className="text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl"
                 >
                   Streetwear
                 </a>
@@ -125,13 +125,13 @@ const Home = () => {
             <div>
               <a
                 href="#"
-                className="group h-96 block bg-gray-100 rounded-lg overflow-hidden shadow-lg mb-2 lg:mb-3"
+                className="group mb-2 block h-96 overflow-hidden rounded-lg bg-gray-100 shadow-lg lg:mb-3"
               >
                 <img
                   src="https://images.unsplash.com/photo-1560269999-cef6ebd23ad3?auto=format&q=75&fit=crop&w=600&h=700"
                   loading="lazy"
                   alt="Photo by Austin Wade"
-                  className="w-full h-full object-cover object-center group-hover:scale-110 transition duration-200"
+                  className="h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
                 />
               </a>
 
@@ -139,7 +139,7 @@ const Home = () => {
                 <span className="text-gray-500">Women</span>
                 <a
                   href="#"
-                  className="text-gray-800 hover:text-gray-500 text-lg lg:text-xl font-bold transition duration-100"
+                  className="text-lg font-bold text-gray-800 transition duration-100 hover:text-gray-500 lg:text-xl"
                 >
                   Sale
                 </a>
@@ -150,40 +150,40 @@ const Home = () => {
       </div>
 
       <div className="bg-white py-6 sm:py-8 lg:py-12">
-        <div className="max-w-screen-xl px-4 md:px-8 mx-auto">
+        <div className="mx-auto max-w-screen-xl px-4 md:px-8">
           <div className="mb-10 md:mb-16">
-            <h2 className="text-gray-800 text-2xl lg:text-3xl font-bold text-center mb-4 md:mb-6">
+            <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">
               Blog
             </h2>
 
-            <p className="max-w-screen-md text-gray-500 md:text-lg text-center mx-auto">
+            <p className="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg">
               This is a section of some simple filler text, also known as
               placeholder text. It shares some characteristics of a real written
               text but is random or otherwise generated.
             </p>
           </div>
 
-          <div className="grid sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-8 sm:gap-12 xl:gap-16">
-            <div className="flex flex-col md:flex-row items-center gap-4 lg:gap-6">
+          <div className="grid gap-8 sm:grid-cols-2 sm:gap-12 lg:grid-cols-2 xl:grid-cols-2 xl:gap-16">
+            <div className="flex flex-col items-center gap-4 md:flex-row lg:gap-6">
               <a
                 href="#"
-                className="group w-full md:w-24 lg:w-40 h-56 md:h-24 lg:h-40 block self-start shrink-0 bg-gray-100 overflow-hidden rounded-lg shadow-lg relative"
+                className="group relative block h-56 w-full shrink-0 self-start overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-24 md:w-24 lg:h-40 lg:w-40"
               >
                 <img
                   src="https://images.unsplash.com/photo-1593508512255-86ab42a8e620?auto=format&q=75&fit=crop&w=600"
                   loading="lazy"
                   alt="Photo by Minh Pham"
-                  className="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200"
+                  className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
                 />
               </a>
 
               <div className="flex flex-col gap-2">
-                <span className="text-gray-400 text-sm">July 19, 2021</span>
+                <span className="text-sm text-gray-400">July 19, 2021</span>
 
-                <h2 className="text-gray-800 text-xl font-bold">
+                <h2 className="text-xl font-bold text-gray-800">
                   <a
                     href="#"
-                    className="hover:text-indigo-500 active:text-indigo-600 transition duration-100"
+                    className="transition duration-100 hover:text-indigo-500 active:text-indigo-600"
                   >
                     New trends in Tech
                   </a>
@@ -197,33 +197,33 @@ const Home = () => {
                 <div>
                   <a
                     href="#"
-                    className="text-indigo-500 hover:text-indigo-600 active:text-indigo-700 font-semibold transition duration-100"
+                    className="font-semibold text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700"
                   >
                     Read more
                   </a>
                 </div>
               </div>
             </div>
-            <div className="flex flex-col md:flex-row items-center gap-4 lg:gap-6">
+            <div className="flex flex-col items-center gap-4 md:flex-row lg:gap-6">
               <a
                 href="#"
-                className="group w-full md:w-24 lg:w-40 h-56 md:h-24 lg:h-40 block self-start shrink-0 bg-gray-100 overflow-hidden rounded-lg shadow-lg relative"
+                className="group relative block h-56 w-full shrink-0 self-start overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-24 md:w-24 lg:h-40 lg:w-40"
               >
                 <img
                   src="https://images.unsplash.com/photo-1550745165-9bc0b252726f?auto=format&q=75&fit=crop&w=600"
                   loading="lazy"
                   alt="Photo by Lorenzo Herrera"
-                  className="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200"
+                  className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
                 />
               </a>
 
               <div className="flex flex-col gap-2">
-                <span className="text-gray-400 text-sm">April 07, 2021</span>
+                <span className="text-sm text-gray-400">April 07, 2021</span>
 
-                <h2 className="text-gray-800 text-xl font-bold">
+                <h2 className="text-xl font-bold text-gray-800">
                   <a
                     href="#"
-                    className="hover:text-indigo-500 active:text-indigo-600 transition duration-100"
+                    className="transition duration-100 hover:text-indigo-500 active:text-indigo-600"
                   >
                     Working with legacy stacks
                   </a>
@@ -237,33 +237,33 @@ const Home = () => {
                 <div>
                   <a
                     href="#"
-                    className="text-indigo-500 hover:text-indigo-600 active:text-indigo-700 font-semibold transition duration-100"
+                    className="font-semibold text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700"
                   >
                     Read more
                   </a>
                 </div>
               </div>
             </div>
-            <div className="flex flex-col md:flex-row items-center gap-4 lg:gap-6">
+            <div className="flex flex-col items-center gap-4 md:flex-row lg:gap-6">
               <a
                 href="#"
-                className="group w-full md:w-24 lg:w-40 h-56 md:h-24 lg:h-40 block self-start shrink-0 bg-gray-100 overflow-hidden rounded-lg shadow-lg relative"
+                className="group relative block h-56 w-full shrink-0 self-start overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-24 md:w-24 lg:h-40 lg:w-40"
               >
                 <img
                   src="https://images.unsplash.com/photo-1542759564-7ccbb6ac450a?auto=format&q=75&fit=crop&w=600"
                   loading="lazy"
                   alt="Photo by Magicle"
-                  className="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200"
+                  className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
                 />
               </a>
 
               <div className="flex flex-col gap-2">
-                <span className="text-gray-400 text-sm">March 15, 2021</span>
+                <span className="text-sm text-gray-400">March 15, 2021</span>
 
-                <h2 className="text-gray-800 text-xl font-bold">
+                <h2 className="text-xl font-bold text-gray-800">
                   <a
                     href="#"
-                    className="hover:text-indigo-500 active:text-indigo-600 transition duration-100"
+                    className="transition duration-100 hover:text-indigo-500 active:text-indigo-600"
                   >
                     10 best smartphones for devs
                   </a>
@@ -277,33 +277,33 @@ const Home = () => {
                 <div>
                   <a
                     href="#"
-                    className="text-indigo-500 hover:text-indigo-600 active:text-indigo-700 font-semibold transition duration-100"
+                    className="font-semibold text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700"
                   >
                     Read more
                   </a>
                 </div>
               </div>
             </div>
-            <div className="flex flex-col md:flex-row items-center gap-4 lg:gap-6">
+            <div className="flex flex-col items-center gap-4 md:flex-row lg:gap-6">
               <a
                 href="#"
-                className="group w-full md:w-24 lg:w-40 h-56 md:h-24 lg:h-40 block self-start shrink-0 bg-gray-100 overflow-hidden rounded-lg shadow-lg relative"
+                className="group relative block h-56 w-full shrink-0 self-start overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-24 md:w-24 lg:h-40 lg:w-40"
               >
                 <img
                   src="https://images.unsplash.com/photo-1610465299996-30f240ac2b1c?auto=format&q=75&fit=crop&w=600"
                   loading="lazy"
                   alt="Photo by Martin Sanchez"
-                  className="w-full h-full object-cover object-center absolute inset-0 group-hover:scale-110 transition duration-200"
+                  className="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110"
                 />
               </a>
 
               <div className="flex flex-col gap-2">
-                <span className="text-gray-400 text-sm">January 27, 2021</span>
+                <span className="text-sm text-gray-400">January 27, 2021</span>
 
-                <h2 className="text-gray-800 text-xl font-bold">
+                <h2 className="text-xl font-bold text-gray-800">
                   <a
                     href="#"
-                    className="hover:text-indigo-500 active:text-indigo-600 transition duration-100"
+                    className="transition duration-100 hover:text-indigo-500 active:text-indigo-600"
                   >
                     8 High performance Notebooks
                   </a>
@@ -317,7 +317,7 @@ const Home = () => {
                 <div>
                   <a
                     href="#"
-                    className="text-indigo-500 hover:text-indigo-600 active:text-indigo-700 font-semibold transition duration-100"
+                    className="font-semibold text-indigo-500 transition duration-100 hover:text-indigo-600 active:text-indigo-700"
                   >
                     Read more
                   </a>
