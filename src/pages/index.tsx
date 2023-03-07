467b5d0acd48ff7b02d8585c51e8c85e205f9a43
Sanshiroh SAKAI <sakai.s@ekzm.co.jp>
2023-03-07 15:35:57 +0900

    更新

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,6 +10,20 @@ const Home: NextPage = () => {
     <>
       <Banner />
       <Header />
+
+      <div className="my-9 flex justify-center gap-2">
+        <div className="h-16 w-16 bg-maroon-50"></div>
+        <div className="h-16 w-16 bg-maroon-100"></div>
+        <div className="h-16 w-16 bg-maroon-200"></div>
+        <div className="h-16 w-16 bg-maroon-300"></div>
+        <div className="h-16 w-16 bg-maroon-400"></div>
+        <div className="h-16 w-16 bg-maroon-500"></div>
+        <div className="h-16 w-16 bg-maroon-600"></div>
+        <div className="h-16 w-16 bg-maroon-700"></div>
+        <div className="h-16 w-16 bg-maroon-800"></div>
+        <div className="h-16 w-16 bg-maroon-900"></div>
+      </div>
+
       <Collections />
       <Blog />
       <Footer />
